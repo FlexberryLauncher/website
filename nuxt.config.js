99c1e47a01b54989f46cf553f4b19f7d66ff6af8
Kuzey Kurtuluş <kuzeeeyk@gmail.com>
2022-08-04 15:40:23 +0300

    opengraph and features background

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,29 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'flexberry-launcher-website',
+    title: 'Flexberry Launcher',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'title', name: 'title', content: 'Flexberry Launcher' },
+      { hid: 'description', name: 'description', content: '✨ Open-source, secure and lightweight launcher for Minecraft. Always up to date with latest Minecraft updates.' },
+      { hid: 'theme-color', name: 'theme-color', content: '#841ee0' },
+
+      { name: 'og:type', content: 'website' },
+      { name: 'og:url', content: 'https://flexberry.app/' },
+      { name: 'og:title', content: 'Flexberry Launcher' },
+      { name: 'og:description', content: '✨ Open-source, secure and lightweight launcher for Minecraft. Always up to date with latest Minecraft updates.' },
+      { name: 'og:image', content: '/metaimage.png' },
+
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:url', content: 'https://flexberry.app/' },
+      { name: 'twitter:title', content: 'Flexberry Launcher' },
+      { name: 'twitter:description', content: '✨ Open-source, secure and lightweight launcher for Minecraft. Always up to date with latest Minecraft updates.' },
+      { name: 'twitter:image', content: '/metaimage.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
